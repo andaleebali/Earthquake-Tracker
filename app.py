0397417087efb36d256d406701cba1a70737e80e
Andaleeb Ali <s1885898@ed.ac.uk>
2025-09-08 20:29:43 +0100

    comments added

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 # app.py
-
 from dash import Dash
 from callbacks import register_callbacks
 from layout import dashboard_layout
 
+# initialise app
 app = Dash(__name__)
 app.title = "NZ Earthquake Tracker"
 
+# set up dashboard layout
 app.layout = dashboard_layout()
 
+# call data and functions
 register_callbacks(app)
 
+# run app
 if __name__ == "__main__":
     app.run(debug=True)
